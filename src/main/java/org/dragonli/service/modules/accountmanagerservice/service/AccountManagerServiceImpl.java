8e056637f1d34abedf916945b866c95fad54df9f
315920958@qq.com <315920958@qq.com>
2019-12-06 22:22:55 +0800

    try

--- src/main/java/org/dragonli/service/modules/accountmanagerservice/service/AccountManagerServiceImpl.java
+++ src/main/java/org/dragonli/service/modules/accountmanagerservice/service/AccountManagerServiceImpl.java
@@ -332,7 +332,7 @@ public class AccountManagerServiceImpl implements AccountManagerService {
         if(adjustment != null)return adjustment.getStatus().name();
         AssetEntity asset = assetRepository.findByCurrency(currency);
         Long accountId = accountCreateExecutor.createAccount(userId,reflexId,asset);
-        accountsRepository.refresh(null);
+        accountsRepository.flush();
         AccountEntity account = accountsRepository.getOne( accountId );
 
         adjustment = new AccountAdjustmentEntity();
